85f3f1d06b83aac4e9f2c060a4024eb53f74047d
Nerakma84 <fnk8476@gmail.com>
2022-12-14 02:06:37 -0600

    updated install.js and database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,12 +1,15 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
+//We are creating a new database named 'jate' which will be using version 1 of the database.
   openDB('jate', 1, {
+    //Upgrade adds our database schema if it has not already been initialized.
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
         console.log('jate database already exists');
         return;
       }
+      //Create a new object store for the data and give it a key name of 'id' which needs to increment automatically.
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
       console.log('jate database created');
     },
