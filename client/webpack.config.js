beaf3ef7559a8e6dc49e144241c2fbdebc3e5cfb
Nerakma84 <fnk8476@gmail.com>
2022-12-15 17:47:56 -0600

    fixed icons for manifest.json

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -31,8 +31,17 @@ module.exports = () => {
         inject: true,
         name: 'J.A.T.E.',
         description: 'Edit text',
+        //background color:
+        //theme-color:
         start_url: './',
         publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
       }),
     ],
 
